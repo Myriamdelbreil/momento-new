59bb8081fe3f7ee0d5756437ba1591ab7a83c735
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-01 17:18:27 +0200

    model participant

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_151652) do
+ActiveRecord::Schema.define(version: 2022_04_01_151815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "trip_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trip_id"], name: "index_participants_on_trip_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
 
   create_table "trips", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -39,5 +48,7 @@ ActiveRecord::Schema.define(version: 2022_04_01_151652) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "participants", "trips"
+  add_foreign_key "participants", "users"
   add_foreign_key "trips", "users"
 end
