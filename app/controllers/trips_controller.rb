2cd95bd9070faaf3e0a4bb6287cb2377cb68eece
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-01 19:32:01 +0200

    début index trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,12 @@
 class TripsController < ApplicationController
+  def index
+    @user = current_user
+    @trips = @user.trip
+    @trip = Trip.new
+    @users = User.all
+    @participant = Participant.new
+    @trips = policy_scope(Trip)
+    @coming_trips = @trips.select {|trip| trip.start_date > Time.now}
+    @past_trips = @trips.select {|trip| trip.start_date < Time.now}
+  end
 end
