3ea8781e21e09522ec79cc5d88e60c5d5f5b22fd
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-01 17:21:18 +0200

    model expense

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_01_151946) do
+ActiveRecord::Schema.define(version: 2022_04_01_152057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "participant_id", null: false
+    t.float "amount"
+    t.string "title"
+    t.boolean "mutual"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["participant_id"], name: "index_expenses_on_participant_id"
+  end
 
   create_table "participants", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -63,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_04_01_151946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "participants"
   add_foreign_key "participants", "trips"
   add_foreign_key "participants", "users"
   add_foreign_key "transportations", "participants"
