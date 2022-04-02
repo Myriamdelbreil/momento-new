8b339366fb676c7bca24847922b8ae58307ae974
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-02 12:59:43 +0200

    index trips ok je pense

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,11 +1,10 @@
 class TripsController < ApplicationController
   def index
     @user = current_user
-    @trips = @user.trip
+    @trips = @user.trips
     @trip = Trip.new
     @users = User.all
     @participant = Participant.new
-    @trips = policy_scope(Trip)
     @coming_trips = @trips.select {|trip| trip.start_date > Time.now}
     @past_trips = @trips.select {|trip| trip.start_date < Time.now}
   end
