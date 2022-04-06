f98ed49f05d307cb5e27ca76c01afe8fdbdbe636
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-06 16:07:34 +0200

    début de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pcoppy = User.create(email:"pcoppy@gmail.com", password:"pcoppy")
+loulou = User.create(email:"loulou@gmail.com", password:"loulou")
+tomtom = User.create(email:"tomtom@gmail.com", password:"tomtom")
+mymy = User.create(email:"mymy@gmail.com", password:"mymymy")
+
+puts "Users créés"
+
+User.all.each do |user|
+  username = user.email.split('@').first
+  trip = Trip.new(user: user, start_date: "2022/04/09", end_date:" 2022/04/30", title:"voyage de #{username.capitalize}", description: "Départ à Bali, Barcelone, Madrid, Pékin très prochain !!!", city:"Peu importe")
+  trip.save
+end
+
+puts '4 voyages créés'
+
+Trip.all.each do |trip|
+  participant = Participant.new(trip: trip, user: trip.user)
+  participant.save
+end
+
+puts "1er participant de chaque voyage créé"
