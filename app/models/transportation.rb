2276df8cb7b573cf5faa802f99021aa57f5a6506
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-06 17:30:51 +0200

    validations model transportations

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -3,6 +3,7 @@ class Transportation < ApplicationRecord
   CATEGORIES = ['bus', 'train', 'plane', 'car', 'various', 'other']
   validates :mean, inclusion: { in: CATEGORIES }
   validates :origin, :destination, presence: true
+  validates :arrival_date, :departure_date, presence: true
   # geocoded_by :origin
   # geocoded_by :destination
   # after_validation :geocode, if: :will_save_change_to_origin?
