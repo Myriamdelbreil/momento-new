2e41c903f4401d953b54b79188999f92fb75e5ce
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-06 17:51:41 +0200

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :trips
+  has_one_attached :photo
 end
-
-#test
