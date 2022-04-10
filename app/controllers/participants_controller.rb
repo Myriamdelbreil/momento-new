1e332d30adc59099e66dd67380f5baeec193a2d4
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-11 00:34:58 +0200

    colonne username to users, seed complétée pour les users, début du controller participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,24 @@
 class ParticipantsController < ApplicationController
+  before_action :find_trip, only: [:index, :create]
+  def find_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def index
+    @participants = @trip.participants
+  end
+
+  def show
+
+  end
+
+  def create
+    @participant = Participant.new(participant_params)
+    @participant.trip = @trip
+    if @participant.save
+      redirect_to trip_path(@trip), notice: "Cool ! #{} rejoint ton voyage !"
+    else
+      render trip_path(@trip), alert: "Oups... Ca n'a pas marché, désolés..."
+    end
+  end
 end
