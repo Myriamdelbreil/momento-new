c2a53c837926d29ccbb5ac0e8447a008fc841f96
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-14 16:15:53 +0200

    flatpickr ok sur le newtrip

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,16 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
 
 const initFlatpickr = () => {
+  // console.log("hello");
+  flatpickr("#range_start", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#range_end"})]
+  });
+}
+
+const initFlatpickr2 = () => {
   // console.log("hello");
   flatpickr(".datepicker", {
     enableTime: true,
@@ -8,4 +18,7 @@ const initFlatpickr = () => {
   });
 }
 
+
 export { initFlatpickr };
+
+export { initFlatpickr2 };
