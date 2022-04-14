720639ef5f1a454ac2230c167837e0f877020244
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-14 23:04:34 +0200

    geocoding a l'air de marcher, manque les markers

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
+  geocoded_by :city
+  after_validation :geocode, if: :geocoding_needed?
   has_many :accomodations, dependent: :destroy
   has_many :events, dependent: :destroy
   has_many :messages, dependent: :destroy
@@ -7,10 +9,8 @@ class Trip < ApplicationRecord
   validates :title, presence: true, length: { maximum: 25 }
   belongs_to :user
   has_one_attached :photo
-  geocoded_by :city
-  after_validation :geocode, if: :geocoding_needed?
 
   def geocoding_needed?
-    will_save_change_to_city? && will_save_change_to_latitude? && will_save_change_to_longitude?
+    will_save_change_to_city? && !will_save_change_to_latitude? && !will_save_change_to_longitude?
   end
 end
