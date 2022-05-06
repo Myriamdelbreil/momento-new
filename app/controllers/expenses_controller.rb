e90e68c6375ea7b7da7cac456cd3df28dccfbd9a
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-06 16:14:31 +0200

    expenses new ok

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,19 @@
 class ExpensesController < ApplicationController
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @new_expense = Expense.new(expense_params)
+    @participant = Participant.find_by(user: current_user, trip: @trip)
+    @new_expense.participant = @participant
+    if @new_expense.save!
+      redirect_to trip_path(@participant.trip), notice: "Dépense ajoutée !"
+    else
+      redirect_to trip_path(@participant.trip), alert: "Ca n'a pas marché, désolés... Réessaie!"
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:title, :amount, :mutual, :participant_id, :expense_date, :trip_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :participants, only: [:create]
     resources :transportations, only: [:index, :create, :destroy, :update]
     resources :events, only: [:create, :destroy]
+    resources :expenses, only: [:index, :update, :show, :destroy, :create]
   end
 
   resources :accomodations, only: [:show, :update] do
@@ -15,7 +16,6 @@ Rails.application.routes.draw do
   end
 
   resources :transportations, only: [:show, :update, :index]
-  resources :expenses, only: [:index, :update, :show, :destroy]
 
   resources :events, only: [:index, :update, :show] do
     resources :event_participants, only: [:create]
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   resources :participants, only: [:destroy] do
     resources :messages, only: [:show, :create]
-    resources :expenses, only: [:index, :create]
+    # resources :expenses, only: [:index, :create]
     resources :accomodations, only: [] do
       resources :accomodation_votes, only: [:new, :create]
     end
