db546c25f5385f62d03ba8f0e0fc5021c88d0425
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-09 14:54:57 +0200

    placeholders index enlevés et remplacés

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -4,8 +4,8 @@ class TripsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "should get trips index" do
-    get 'trips#index_url'
-    assert_response :success
-    end
+  test "should not save trip without title" do
+    trip = Trip.new
+    assert_not trip.save, "Saved the trip without a title"
+  end
 end
