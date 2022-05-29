5abafae63ca750051ed81d76e191d574cc3efa99
Myriam Delbreil <myriam.delbreil@live.fr>
2022-05-29 23:21:52 +0200

    flatpickr remarche dans le form events

---
+++ app/javascript/plugins/flatpickr3.js
@@ -0,0 +1,10 @@
+// const initFlatpickr3 = () => {
+//   console.log("helloflatpickr3");
+//   flatpickr("#range_start_2", {
+//     altInput: true,
+//     plugins: [new rangePlugin({ input: "#range_end_2"})],
+//     disableMobile: "true",
+//     enableTime: true,
+//   });
+// }
+// export { initFlatpickr3 };
